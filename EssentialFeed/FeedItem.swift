c4e677dd51d533549a29d2fb2f3eabede2607c8b
Jero Sanchez <jero.sanchez@icloud.com>
2021-03-27 12:12:04 +0100

    Create `FeedItem` model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright (c) 2021 Jero Sanchez. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
