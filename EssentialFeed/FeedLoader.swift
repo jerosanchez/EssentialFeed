4daccacd05ef2ce436a76d26455d06c62b2ffcc1
Jero Sanchez <jero.sanchez@icloud.com>
2021-03-27 12:15:44 +0100

    Create `LoadFeed` feature boundary protocol

---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright (c) 2021 Jero Sanchez. All rights reserved.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case failure(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
